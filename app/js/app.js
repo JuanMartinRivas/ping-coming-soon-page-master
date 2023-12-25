77f93a65b829bc7fcf266bcf7c19a8118aa9c14d
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-12-25 11:44:50 -0300

    Writing custom error message script

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,4 @@
 import '../scss/style.scss';
+import '../js/mail.js'
 
 console.log('Parcel is working. Hurray!');
---
+++ app/js/mail.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.form');
+const email = document.querySelector('.email');
+const errorMessage = document.querySelector('.error-message');
+const submitButton = document.querySelector('.submit-btn');
+
+submitButton.addEventListener('click', displayErr);
+
+function displayErr() {
+
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("Please provide a valid email address");
+    } else if (email.value.length === 0) {
+        email.setCustomValidity("Whoops! It looks like you forgot to add your email");
+    } else {
+        email.setCustomValidity("");
+    }
+}
