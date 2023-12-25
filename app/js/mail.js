f93b23da78fa0c72323eb1583adf90b7805b8a2f
JuanMartinRivas <jmartinrivas14@gmail.com>
2023-12-25 16:26:04 -0300

    Styling main section

--- app/js/mail.js
+++ app/js/mail.js
@@ -4,13 +4,18 @@ const errorMessage = document.querySelector('.error-message');
 const submitButton = document.querySelector('.submit-btn');
 
 submitButton.addEventListener('click', displayErr);
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
 
 function displayErr() {
 
     if (email.validity.typeMismatch) {
         errorMessage.textContent = "Please provide a valid email address";
+        email.style.border = "solid 1px hsl(354, 100%, 66%)"
     } else if (email.value.length === 0) {
         errorMessage.textContent = "Whoops! It looks like you forgot to add your email";
+        email.style.border = "solid 1px hsl(354, 100%, 66%)"
     } else {
         errorMessage.textContent = "";
     }
